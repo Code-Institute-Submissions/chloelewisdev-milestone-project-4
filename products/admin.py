f1f0219b35728dd05fb8af4aa3c0d61079021ada
chloelewisdev <baldwinchloe@hotmail.com>
2021-05-11 11:47:19 +0000

    Products app set up, along with models and admin

---
+++ products/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'description',
+        'price',
+        'rating',
+        'image'
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
