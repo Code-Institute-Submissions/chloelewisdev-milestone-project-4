643382cca0e79e1811459d0ebaa466480e82cc53
chloelewisdev <baldwinchloe@hotmail.com>
2021-05-13 12:54:18 +0000

    Added sort selector js and back to top button

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
-# Create your views here.
 
 def all_products(request):
     """ A view to show individual hamper details """
@@ -49,7 +49,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories' : categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
 
     }
@@ -63,7 +63,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product' : product,
+        'product': product,
     }
     
     return render(request, 'products/product_detail.html', context)
