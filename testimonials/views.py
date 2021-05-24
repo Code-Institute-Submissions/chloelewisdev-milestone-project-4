dcf8d96fac33ce7f1e4a7a65cdd898fe1fed4038
chloelewisdev <baldwinchloe@hotmail.com>
2021-05-24 13:47:19 +0000

    Add testimonial functionality added

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Testimonial
+from .forms import TestimonialForm
 
 
 def testimonials(request):
@@ -14,3 +17,31 @@ def testimonials(request):
     }
 
     return render(request, 'testimonials/testimonials.html', context)
+
+
+@login_required
+def add_testimonial(request):
+    """
+    A view for users with a profile to add
+    a testimonial to the testimonials page
+    """
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you! Your testimonial\
+                                      has been added successfully!')
+            return redirect(reverse('testimonials'))
+        else:
+            messages.error(request, 'Failed to add testimonial.\
+                                    Please ensure the form is valid.')
+    else:
+        form = TestimonialForm
+    
+    form = TestimonialForm
+    template = 'testimonials/add_testimonial.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
