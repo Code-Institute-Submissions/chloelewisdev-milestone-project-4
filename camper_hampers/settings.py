b8ed5422a8e09174a5ded8d2c9fcc1ac94420462
chloelewisdev <baldwinchloe@hotmail.com>
2021-05-25 23:19:42 +0000

    Removed secret key and set to debug

--- camper_hampers/settings.py
+++ camper_hampers/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!8sei94q7e_8ljduz4(9oj*%kri8mqr(dtq=*$oom6so@l#__l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['camperhampers.herokuapp.com', 'localhost']
 
